13fdca1e8568685d65c1fe2b68cad0144124a4f8
KRISH KHANNA <krishkhanna00@gmail.com>
2024-08-24 23:30:56 +0530

    commiting yeahhhhh

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
 
 //this will be the backend of kaizen-cart project
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT;
+
+//
+// the database should be in this manner
+//
+// interface ItemData {
+//     itemNumber: number;
+//     itemName: string;
+//     itemPrice: number;
+//     category: string;
+//     description: string; }
+//
+
+app.post("/item/create", (req, res) => {
+  let data = req.body;
+  console.log("data " + data);
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
